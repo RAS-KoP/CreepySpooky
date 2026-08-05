06653db7585e9c4636e3d9bc3f609d9fda3e6390
UjitaR <rk.ujita@gmail.com>
2026-08-05 17:21:40 +0900

    [add] 一旦TestEntityとほぼ同じものを実装

--- src/main/java/io/github/ras_kop/creepyspooky/entity/EnergyWispEntity.java
+++ src/main/java/io/github/ras_kop/creepyspooky/entity/EnergyWispEntity.java
@@ -1,34 +1,28 @@
 package io.github.ras_kop.creepyspooky.entity;
 
-import software.bernie.geckolib.animatable.GeoEntity;
-import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
-import software.bernie.geckolib.animation.AnimatableManager;
-import software.bernie.geckolib.animation.AnimationState;
-import software.bernie.geckolib.animation.AnimationController;
-import software.bernie.geckolib.animation.PlayState;
-import software.bernie.geckolib.animation.RawAnimation;
-import software.bernie.geckolib.util.GeckoLibUtil;
-
-
-
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.animation.RawAnimation;
+import software.bernie.geckolib.util.GeckoLibUtil;
 
 
-public class TestEntity extends PathfinderMob implements GeoEntity{
+public class EnergyWispEntity extends PathfinderMob implements GeoEntity{
+    
+    public static final String ENTITY_ID = "energy_wisp";
 
-    public static final String ENTITY_ID = "test_entity";
-
-
-
-    public TestEntity(EntityType<? extends PathfinderMob> type, Level level) {
+    public EnergyWispEntity(EntityType<? extends PathfinderMob> type, Level level) {
         super(type, level);
     }
-
 
     private static final RawAnimation ANIMATION =
         RawAnimation.begin().thenLoop("animation");
@@ -42,19 +36,18 @@ public class TestEntity extends PathfinderMob implements GeoEntity{
                 this,
                 "animation",
                 0,
-                this::TestAnimationController
+                this::FryingAnimationController
             )
         ); 
     }
-    
-    protected <E extends TestEntity> PlayState TestAnimationController(final AnimationState<E> event) {
+
+    protected <E extends EnergyWispEntity> PlayState FryingAnimationController(final AnimationState<E> event) {
         return event.setAndContinue(ANIMATION);
     }
 
 
     private final AnimatableInstanceCache cache =
         GeckoLibUtil.createInstanceCache(this);
-
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
@@ -64,7 +57,6 @@ public class TestEntity extends PathfinderMob implements GeoEntity{
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
             .add(Attributes.MAX_HEALTH, 20)
-            .add(Attributes.MOVEMENT_SPEED, 0.25)
-            .add(Attributes.ATTACK_DAMAGE, 4);
+            .add(Attributes.MOVEMENT_SPEED, 0.25);
     }
 }
