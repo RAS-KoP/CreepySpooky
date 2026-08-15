822bb4c467d3234a3b95d1d770994372f4509da6
UjitaR <rk.ujita@gmail.com>
2026-08-15 22:20:05 +0900

    [add/change] アイテムレジスター、クリエイティブタブレジスターを追加、かつModメインファイルから該当項目を削除し、新しくレジスターで実装

---
+++ src/main/java/io/github/ras_kop/creepyspooky/register/CreativeTabRegister.java
@@ -0,0 +1,29 @@
+package io.github.ras_kop.creepyspooky.register;
+
+import io.github.ras_kop.creepyspooky.CreepySpooky;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class CreativeTabRegister {
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "creepyspooky" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = 
+        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreepySpooky.MODID);
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.creepyspooky")) //The language key for the title of your CreativeModeTab
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> ItemRegister.EXAMPLE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(ItemRegister.EXAMPLE_ITEM.get());// Add the example item to the tab. For your own tabs, this method is preferred over the event
+            }).build());
+}
---
+++ src/main/java/io/github/ras_kop/creepyspooky/register/ItemRegister.java
@@ -0,0 +1,37 @@
+package io.github.ras_kop.creepyspooky.register;
+
+import io.github.ras_kop.creepyspooky.CreepySpooky;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ItemRegister {
+
+    public static final DeferredRegister.Items ITEMS =
+        DeferredRegister.createItems(CreepySpooky.MODID);
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = 
+        ITEMS.registerSimpleBlockItem("example_block", BlockRegister.EXAMPLE_BLOCK);
+
+    public static final DeferredItem<Item> EXAMPLE_ITEM = 
+        ITEMS.registerSimpleItem(
+            "example_item", 
+            new Item.Properties().food(
+                new FoodProperties
+                    .Builder()
+                    .alwaysEdible()
+                    .nutrition(1)
+                    .saturationModifier(2f)
+                    .build()
+            )
+        );
+
+}
