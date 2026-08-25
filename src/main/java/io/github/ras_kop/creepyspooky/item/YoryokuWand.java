c4b91911a5fe5e0ed6b4c196f347094a71f79f03
atama-juice-J <yoshida.head.7320@gmail.com>
2026-08-25 22:55:17 +0900

    [feat] 歩行アニメーションの適用

--- src/main/java/io/github/ras_kop/creepyspooky/item/YoryokuWand.java
+++ src/main/java/io/github/ras_kop/creepyspooky/item/YoryokuWand.java
@@ -3,13 +3,23 @@ package io.github.ras_kop.creepyspooky.item;
 import java.util.function.Consumer;
 
 import io.github.ras_kop.creepyspooky.renderer.item.YoryokuWandRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.animatable.client.GeoRenderProvider;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager.ControllerRegistrar;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.PlayState;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.constant.DefaultAnimations;
 import software.bernie.geckolib.util.GeckoLibUtil;
+import software.bernie.geckolib.util.RenderUtil;
+
 
 public class YoryokuWand extends Item implements GeoItem {
 
@@ -24,7 +34,27 @@ public class YoryokuWand extends Item implements GeoItem {
 
     @Override
     public void registerControllers(ControllerRegistrar controllers) {
-        
+        controllers.add(
+            new AnimationController<>(
+                this,
+                "Walking",
+                5,
+                state -> {
+                    LocalPlayer player = Minecraft.getInstance().player;
+    
+                    if (player == null) {
+                        return PlayState.STOP;
+                    }
+    
+                    boolean isMoving =
+                        player.getDeltaMovement().horizontalDistanceSqr() > 0.0001;
+    
+                    return isMoving
+                        ? state.setAndContinue(DefaultAnimations.WALK)
+                        : PlayState.STOP;
+                }
+            )
+        );
     }
 
     @Override
@@ -46,5 +76,11 @@ public class YoryokuWand extends Item implements GeoItem {
             }
         );
     }
+
+    @Override
+    public double getTick(Object itemStack) {
+        return RenderUtil.getCurrentSystemTick();
+    }
+
     
 }
