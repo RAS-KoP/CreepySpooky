bdb09d76789b0944840fa88aecbed16f47b70013
UjitaR <rk.ujita@gmail.com>
2026-08-30 17:34:24 +0900

    [add/change] DataComponent系を追加した かつ、レジスター系の処理をひとまとめにした

--- src/main/java/io/github/ras_kop/creepyspooky/CreepySpookyClient.java
+++ src/main/java/io/github/ras_kop/creepyspooky/CreepySpookyClient.java
@@ -13,7 +13,11 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = CreepySpooky.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = CreepySpooky.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(
+    modid = CreepySpooky.MODID, 
+    value = Dist.CLIENT
+)
+
 public class CreepySpookyClient {
     public CreepySpookyClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
---
+++ src/main/java/io/github/ras_kop/creepyspooky/date/ModDataComponents.java
@@ -0,0 +1,43 @@
+package io.github.ras_kop.creepyspooky.date;
+
+import javax.annotation.Nonnull;
+
+import com.mojang.serialization.Codec;
+
+import io.github.ras_kop.creepyspooky.CreepySpooky;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModDataComponents {
+
+    public static void register(@Nonnull IEventBus modEventBus){
+        DATA_COMPONENTS.register(modEventBus);
+    }
+    
+    @SuppressWarnings("null")
+    public static final DeferredRegister.DataComponents DATA_COMPONENTS =
+        DeferredRegister.createDataComponents(
+            Registries.DATA_COMPONENT_TYPE,
+            CreepySpooky.MODID
+        );
+
+
+    @SuppressWarnings("null")
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> YORYOKU_ENERGY =
+        DATA_COMPONENTS.registerComponentType(
+            "yoryoku_energy",
+            builder -> builder.persistent(Codec.INT)
+        );
+        
+
+    @SuppressWarnings("null")
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<BlockPos>> STORED_BLOCK_POS =
+        DATA_COMPONENTS.registerComponentType(
+            "stored_block_pos",
+            builder -> builder.persistent(BlockPos.CODEC)
+        );
+}
